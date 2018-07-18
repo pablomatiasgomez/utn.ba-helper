55782c3e1755b0561f13265c45e2962d422397f4
Pablo Matías Gomez <pablomatiasgomez@gmail.com>
2018-07-18 02:01:13 -0300

    fix readmode

--- js/ListadoMateriasPage.js
+++ js/ListadoMateriasPage.js
@@ -1,27 +1,11 @@
 var ListadoMateriasPage = function(pagesDataParser) {
 
-	var stylesForZoomOut = function(zoomOut) {
-		let widthPercentage = (1 / zoomOut) * 100;
-		let height = parseInt(1000 * widthPercentage / 100);
-		return `
-			width: ${widthPercentage}%;
-			height: ${height}px;
-			-ms-zoom: ${zoomOut};
-			-moz-transform: scale(${zoomOut});
-			-moz-transform-origin: 0 0;
-			-o-transform: scale(${zoomOut});
-			-o-transform-origin: 0 0;
-			-webkit-transform: scale(${zoomOut});
-			-webkit-transform-origin: 0 0;
-		`;
-	};
-
 	var setIframe = function() {
 		pagesDataParser.getSubjects().then(subjects => {
 			let approved = encodeURIComponent(subjects.approved.join(","));
 			let signed = encodeURIComponent(subjects.signed.join(","));
-			let iframeUrl = `http://www.materiasutn.com/?approved=${approved}&signed=${signed}&readOnly=true`;
-			$(".std-canvas").prepend(`<iframe style="${stylesForZoomOut(0.80)}" frameBorder="0" src='${iframeUrl}' />`);
+			let iframeUrl = `http://www.materiasutn.com/?approved=${approved}&signed=${signed}&readMode=true`;
+			$(".std-canvas").prepend(`<iframe width="100%" height="1000" frameBorder="0" src='${iframeUrl}' />`);
 		});
 	};
 
