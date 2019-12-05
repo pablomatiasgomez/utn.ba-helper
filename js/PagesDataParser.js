eaa28612a3b704656a1339c76916db06d9c2bffa
Pablo Matías Gomez <pablomatiasgomez@gmail.com>
2019-12-05 14:53:58 -0300

    read surveys

--- js/PagesDataParser.js
+++ js/PagesDataParser.js
@@ -82,6 +82,76 @@ var PagesDataParser = function(dataTracker) {
 		});
 	};
 
+	var getSentSurveys = function() {
+		return getPageContents("/alu/encdoc.do").then(responseText => {
+			return $(responseText).find(".std-canvas .tab")
+				.toArray()
+				.flatMap(elem => {
+					let course = $(elem).find("p").text().split(" ");
+					let time = $(elem).prevAll("p").first().text().replace("_Encuesta", "").trim();
+					let courseCode = course[0];
+					let subjectCode = course[1];
+					return $(elem).find("table tr")
+						.toArray()
+						.filter(elem => {
+							let isCompleted = $(elem).find("td:eq(4) img").length > 0;
+							return isCompleted;
+						})
+						.map(elem => {
+							// onclick string is something like this:
+							// "if(fn_encuesta(51,36218,52143,'Z3574 [950309] Economía','[JEFE DE TP] GALLONI GUILLEN, ROLANDO')){return jslib_submit(null,'/alu/encdocpop.do','popup',null,false );} else return false;";
+							// fn_encuesta function are like this:
+							// function fn_encuesta(form_iden,form_idcu,form_iddo,form_curnom,form_docnom){
+							let jsOnClick = $(elem).find("td:eq(3) a").attr("onclick");
+
+							// 3 int params, and 2 string params.
+							let FN_ENCUESTA_ONCLICK_REGEX = /^if\(fn_encuesta\((\d+)\,(\d+)\,(\d+)\,\'(.*)\'\,\'(.*)\'\)\){return\ jslib_submit/;
+							let match = FN_ENCUESTA_ONCLICK_REGEX.exec(jsOnClick);
+							let form_iden = parseInt(match[1]);
+							let form_idcu = parseInt(match[2]);
+							let form_iddo = parseInt(match[3]);
+							let form_curnom = match[4];
+							let form_docnom = match[5];
+
+							$.ajax({
+								method: "POST",
+								url: "/alu/encdocpop.do",
+								data: {
+									form_submit: 0,
+									form_iden: form_iden,
+									form_idcu: form_idcu,
+									form_iddo: form_iddo,
+									form_curnom: form_curnom,
+									form_docnom: form_docnom
+								}
+							}).then(responseText => {
+								let values = $(responseText).find(".std-canvas table tr")
+									.toArray()
+									.map(row => {
+										let comboValue = $(row).find("td select option:selected").text();
+										if (!comboValue) return false;
+										let answer = {
+											question: $(row).find("td").text(),
+											value: comboValue
+										}
+										if (["Rta Libre", "Libre"].indexOf(comboValue) !== -1) {
+											answer.text = $(row).next().find("textarea").val();
+										}
+										return answer;
+									})
+									.filter(answer => !!answer);
+								console.log(values);
+								debugger;
+							});
+						});
+				});
+		}).catch(e => {
+			trackError(e, "getSentSurveys");
+			throw e;
+		});
+	};
+
+
 	var trackError = function(error, methodName) {
 		console.log(error);
 		dataTracker.logError(methodName, error);
