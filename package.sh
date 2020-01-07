6e9a8624a72251f769ea8d25f88caf6653e43e8a
Pablo Matías Gomez <pablomatiasgomez@gmail.com>
2020-01-07 16:00:00 -0300

    fix package

--- package.sh
+++ package.sh
@@ -3,6 +3,6 @@
 rm out.zip
 mv js/siga-helper.min.js js/siga-helper.min.js.bk
 minify js/siga-helper.min.js.bk --out-file js/siga-helper.min.js --builtIns false
-zip -vr out.zip css/ images/ js/siga-helper.min.js manifest.json
+zip -vr out.zip css/ images/ js/siga-helper.min.js js/background.js manifest.json
 rm js/siga-helper.min.js
 mv js/siga-helper.min.js.bk js/siga-helper.min.js
