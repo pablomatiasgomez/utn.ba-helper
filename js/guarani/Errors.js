dac7ed5a8daa9a0408075c3f70580b8fa6c8cdee
Pablo Matias Gomez <pablomatiasgomez@gmail.com>
2022-04-14 00:21:07 -0300

    Typo

--- js/guarani/Errors.js
+++ js/guarani/Errors.js
@@ -9,7 +9,7 @@ LoggedOutError.prototype = new Error;
 // GuaraniBackendError is thrown when the Guarani's server is not working correctly.
 function GuaraniBackendError(error) {
 	this.name = 'GuaraniBackendError';
-	this.message = "Guarani's backend throw an error";
+	this.message = "Guarani's backend error";
 	this.stack = (new Error()).stack;
 	this.error = error;
 }
