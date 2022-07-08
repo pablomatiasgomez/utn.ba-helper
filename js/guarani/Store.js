9403f417ccde32e6941e7f80a02ee4b41926f01c
Pablo Matias Gomez <pablomatiasgomez@gmail.com>
2022-07-08 19:41:00 -0300

    fix full storage

--- js/guarani/Store.js
+++ js/guarani/Store.js
@@ -1,19 +1,20 @@
 if (!window.UtnBaHelper) window.UtnBaHelper = {};
 UtnBaHelper.Store = function () {
 
+	const SURVEY_FORMS_DATA_STORE_KEY = "UtnBaHelper.SurveyFormData";
 	const HASHED_STUDENT_ID_DATASTORE_KEY = "UtnBaHelper.HashedStudentId";
 
 	let readHashedStudentIdFromStore = function () {
-		return new Promise((resolve, reject) => {
-			chrome.storage.sync.get(HASHED_STUDENT_ID_DATASTORE_KEY, function (result) {
-				if (chrome.runtime.lastError) return reject(chrome.runtime.lastError);
-				resolve(result[HASHED_STUDENT_ID_DATASTORE_KEY]);
-			});
+		return chrome.storage.sync.get(HASHED_STUDENT_ID_DATASTORE_KEY).then(result => {
+			return result[HASHED_STUDENT_ID_DATASTORE_KEY];
 		});
 	};
 
 	let saveHashedStudentIdToStore = function (hashedStudentId) {
-		return chrome.storage.sync.set({[HASHED_STUDENT_ID_DATASTORE_KEY]: hashedStudentId});
+		// TODO we could remove this in some days, this is to avoid uses that have the storage full (because of old urls..)
+		return chrome.storage.sync.remove(SURVEY_FORMS_DATA_STORE_KEY).then(() => {
+			return chrome.storage.sync.set({[HASHED_STUDENT_ID_DATASTORE_KEY]: hashedStudentId});
+		});
 	};
 
 	// Public
