f6d48d7adde19535a7d054d46e65d5cb9ba29b29
Pablo Matias Gomez <pablomatiasgomez@gmail.com>
2022-08-01 19:27:29 -0300

    separate sync deletion from setter

--- js/guarani/Store.js
+++ js/guarani/Store.js
@@ -1,8 +1,11 @@
 if (!window.UtnBaHelper) window.UtnBaHelper = {};
 UtnBaHelper.Store = function () {
 
-	const SURVEY_FORMS_DATA_STORE_KEY = "UtnBaHelper.SurveyFormData";
 	const HASHED_STUDENT_ID_DATASTORE_KEY = "UtnBaHelper.HashedStudentId";
+
+	// TODO we could remove this in some days, this is to avoid uses that have the storage full (because of old urls..)
+	// noinspection JSIgnoredPromiseFromCall
+	chrome.storage.sync.remove("UtnBaHelper.SurveyFormData")
 
 	let readHashedStudentIdFromStore = function () {
 		return chrome.storage.sync.get(HASHED_STUDENT_ID_DATASTORE_KEY).then(result => {
@@ -11,10 +14,7 @@ UtnBaHelper.Store = function () {
 	};
 
 	let saveHashedStudentIdToStore = function (hashedStudentId) {
-		// TODO we could remove this in some days, this is to avoid uses that have the storage full (because of old urls..)
-		return chrome.storage.sync.remove(SURVEY_FORMS_DATA_STORE_KEY).then(() => {
-			return chrome.storage.sync.set({[HASHED_STUDENT_ID_DATASTORE_KEY]: hashedStudentId});
-		});
+		return chrome.storage.sync.set({[HASHED_STUDENT_ID_DATASTORE_KEY]: hashedStudentId});
 	};
 
 	// Public
