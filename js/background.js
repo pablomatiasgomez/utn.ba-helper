918e1c92c4af70b0b7f794ca54ff8a37c359f911
Pablo Matias Gomez <pablomatiasgomez@gmail.com>
2025-01-21 19:04:41 -0300

    Handle 429 from background api calls

--- js/background.js
+++ js/background.js
@@ -1,5 +1,18 @@
 chrome.runtime.onMessage.addListener(function (requestInfo, sender, resolve) {
-	fetch(requestInfo.url, requestInfo).then(response => {
+	requestFetch(requestInfo).then(response => {
+		resolve(response);
+	}).catch(e => {
+		resolve({
+			// Need to do .toString() as Error is not "JSON-ifiable" and may get erased.
+			errorStr: `Error executing ${requestInfo.method || "GET"} ${requestInfo.url} - ${e.toString()}`
+		});
+	});
+
+	return true;
+});
+
+const requestFetch = function (requestInfo) {
+	return fetch(requestInfo.url, requestInfo).then(response => {
 		if (response.ok) {
 			let contentType = response.headers.get("content-type");
 			let isJson = contentType && contentType.indexOf("application/json") !== -1;
@@ -12,17 +25,20 @@ chrome.runtime.onMessage.addListener(function (requestInfo, sender, resolve) {
 				return response.text();
 			}
 		} else {
+			if (response.status === 429) {
+				console.warn(`Got 429 for ${requestInfo.url}, retrying in 1 second...`);
+				return Promise.resolve().then(delay(2000)).then(() => {
+					return requestFetch(requestInfo);
+				});
+			}
+
 			return response.text().then(body => {
 				throw new Error(`Unexpected ResponseStatus: ${response.status} - ResponseBody: ${body}`);
 			});
 		}
-	}).then(response => {
-		resolve(response);
-	}).catch(e => {
-		resolve({
-			// Need to do .toString() as Error is not "JSON-ifiable" and may get erased.
-			errorStr: `Error executing ${requestInfo.method || "GET"} ${requestInfo.url} - ${e.toString()}`
-		});
 	});
-	return true;
-});
+};
+
+const delay = (delayMs) => {
+	return result => new Promise(resolve => setTimeout(() => resolve(result), delayMs));
+}
