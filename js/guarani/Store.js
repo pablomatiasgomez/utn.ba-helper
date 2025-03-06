2d3ccc2b53e4fc638349ff4c8d1531f2112076ea
Pablo Matias Gomez <pablomatiasgomez@gmail.com>
2025-03-06 15:52:12 -0300

    Remove old localstorage key

--- js/guarani/Store.js
+++ js/guarani/Store.js
@@ -3,10 +3,6 @@ UtnBaHelper.Store = function () {
 
 	const HASHED_STUDENT_ID_DATASTORE_KEY = "UtnBaHelper.HashedStudentId";
 
-	// TODO we could remove this in some days, this is to avoid uses that have the storage full (because of old urls..)
-	// noinspection JSIgnoredPromiseFromCall
-	chrome.storage.sync.remove("UtnBaHelper.SurveyFormData")
-
 	let readHashedStudentIdFromStore = function () {
 		return chrome.storage.sync.get(HASHED_STUDENT_ID_DATASTORE_KEY).then(result => {
 			return result[HASHED_STUDENT_ID_DATASTORE_KEY];
