ade1acd1053cd67d0addec3d070d8feb44be4529
Pablo Matias Gomez <pablomatiasgomez@gmail.com>
2025-03-09 04:48:11 -0300

    format code

--- js/guarani/pages/InscripcionAExamenesPage.js
+++ js/guarani/pages/InscripcionAExamenesPage.js
@@ -8,23 +8,23 @@ redirectLink.textContent = "AQUÍ"
 UtnBaHelper.InscripcionAExamenesPage = function () {
 
 
-    const errorMessageSelector = document.querySelector('#lista_materias > div.alert.info.strong')
+	const errorMessageSelector = document.querySelector('#lista_materias > div.alert.info.strong')
 
-    function replaceMessageIfExists() {
-        if (errorMessageSelector) {
-            errorMessageSelector.appendChild(textChild)
-            errorMessageSelector.appendChild(redirectLink)
-        }
-    }
+	function replaceMessageIfExists() {
+		if (errorMessageSelector) {
+			errorMessageSelector.appendChild(textChild)
+			errorMessageSelector.appendChild(redirectLink)
+		}
+	}
 
 
-    return {
-        init: function () {
-            return Promise.resolve().then(() => {
-                replaceMessageIfExists();
-            });
-        },
-        close: function () {
-        },
-    };
+	return {
+		init: function () {
+			return Promise.resolve().then(() => {
+				replaceMessageIfExists();
+			});
+		},
+		close: function () {
+		},
+	};
 };
