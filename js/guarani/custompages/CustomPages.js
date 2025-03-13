13ad4fee28b4cf47d91b741c99641f6a446a272f
Mariano <perdomo.mariano@gmail.com>
2025-03-13 00:50:02 -0300

    Typos + Aclaración (#60)
    * typos
    
    * Aclaración, link
    
    * aclaraciones
    
    * Abrir en nueva tab

--- js/guarani/custompages/CustomPages.js
+++ js/guarani/custompages/CustomPages.js
@@ -29,9 +29,9 @@ UtnBaHelper.CustomPages = function (pagesDataParser, dataCollector, utils, apiCo
 				<div class="alert info">
 					<h3 style="text-align: center;">UTN.BA HELPER - Información importante</h3>
 					<p><b>Esta sección es provista por el "UTN.BA Helper" y no es parte del sistema de la UTN.</b></p>
-					<p>La información presentada en esta sección proviene de datos colectados de los usuarios que poseen la extensión, y de informacion generada por la misma extension, por lo cual puede estar incompleta y/o errónea. <br>
+					<p>La información presentada en esta sección proviene de datos colectados de los usuarios que poseen la extensión, y de información generada por la misma extensión, por lo cual puede estar incompleta y/o errónea. <br>
 					Ninguno de los datos presentados en esta sección proviene del sistema de la UTN, por lo que debe ser usada bajo su propia interpretación.</p>
-					<p>Tener en cuenta que en los casos de encuestas, la informacion colectada es una muestra parcial del total real, y por ende en casos donde la muestra es muy baja, puede implicar que los resultados estén alejados de la realidad.</p>
+					<p>Tener en cuenta que en los casos de encuestas, la información colectada es una muestra parcial del total real, y por ende en casos donde la muestra es muy baja, puede implicar que los resultados estén alejados de la realidad.</p>
 				</div>
 				<div class="main">
 					<div class="titulo_operacion"><h2 class="clearfix"><span class="pull-left">${customPage.menuName}</span></h2></div>
