c068964469aff1b04d26783cbb56379897e5339a
Pablo Matias Gomez <pablomatiasgomez@gmail.com>
2025-03-14 11:36:15 -0300

    Fix for K23

--- js/guarani/PagesDataParser.js
+++ js/guarani/PagesDataParser.js
@@ -165,8 +165,9 @@ UtnBaHelper.PagesDataParser = function (utils) {
 			"módulo: quinto nivel": 5,
 			"módulo: sexto nivel": 6,
 			"módulo: sexto año": 6,
-			// If we enable this, we also need to resolve how to parse and consider dependencies with "Opción 2"
-			"módulo: cuarto analista": -1,
+			// Not supported:
+			"módulo: cuarto analista": -1, // If we enable this, we also need to resolve how to parse and consider dependencies with "Opción 2"
+			"módulo: título intermedio": -1, // don't have support for this as of now in the BE.
 		};
 		const kindsMapping = {
 			"Para cursar": "REGISTER",
