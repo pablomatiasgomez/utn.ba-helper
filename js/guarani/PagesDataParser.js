48532be843e3b594ddc01cb8c51a94ca4a23f979
Pablo Matias Gomez <pablomatiasgomez@gmail.com>
2025-07-03 20:28:04 -0300

    error-prone reading of the studentId

--- js/guarani/PagesDataParser.js
+++ js/guarani/PagesDataParser.js
@@ -110,7 +110,7 @@ UtnBaHelper.PagesDataParser = function (utils) {
 	 * @returns {Promise<String>}
 	 */
 	let getStudentId = function () {
-		let studentId = document.querySelector(".legajo-container .legajo-numero").textContent.trim();
+		let studentId = document.querySelector(".legajo-container .legajo-numero")?.textContent.trim() || "";
 		if (studentId === "-.") throw new MissingStudentIdError(`Missing studentId: ${studentId}`);
 		if (studentId[studentId.length - 2] !== "-" || studentId[studentId.length - 6] !== ".") throw new Error(`Invalid studentId: ${studentId}. HTML: ${document.querySelector("html").innerHTML}`);
 		return Promise.resolve(studentId);
