9f6dadeb498fab4d1ae43dd0c9a47fcbf86855b8
Pablo Matias Gomez <pablomatiasgomez@gmail.com>
2025-11-29 22:53:52 -0300

    fix

--- jest.config.js
+++ jest.config.js
@@ -93,7 +93,9 @@ const config = {
   // ],
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
-  // moduleNameMapper: {},
+  moduleNameMapper: {
+    '\\.(css|less)$': '<rootDir>/src/__mocks__/styleMock.js',
+  },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
   // modulePathIgnorePatterns: [],
---
+++ src/__mocks__/styleMock.js
@@ -0,0 +1,2 @@
+// noinspection JSUnusedGlobalSymbols
+export default {};
