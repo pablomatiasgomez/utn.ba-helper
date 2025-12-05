5b1ada0313ec9dd6a8efd10ef1bf007b6820733c
Pablo Matias Gomez <pablomatiasgomez@gmail.com>
2025-12-05 17:17:36 -0300

    better tracking of scripts

--- src/guarani/Utils.js
+++ src/guarani/Utils.js
@@ -89,7 +89,7 @@ export class Utils {
 	 */
 	detectScripts() {
 		[...document.querySelectorAll("script")]
-			.filter(script => script.textContent.includes(chrome.runtime.id))
+			.filter(script => script.textContent.includes(chrome.runtime.id) || script.textContent.includes("Por favor desactiva"))
 			.forEach(script => {
 				let contents = script.textContent;
 				log.message("DetectedScript", 'info', {attributes: {contents: contents,}});
